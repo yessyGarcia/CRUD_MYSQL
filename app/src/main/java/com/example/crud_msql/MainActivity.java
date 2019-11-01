ff139b3c56a957973ab1f744b082fc0ff6d74069
yessyGarcia <yesseniagarcia989@gmail.com>
2019-10-31 21:24:37 -0600

    MainActivity

--- app/src/main/java/com/example/crud_msql/MainActivity.java
+++ app/src/main/java/com/example/crud_msql/MainActivity.java
@@ -1,25 +1,276 @@
 package com.example.crud_msql;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et_codigo, et_descripcion, et_precio;
+    private Button btn_guardar, btn_consultaCodigo, btn_consultaDescripcion, btn_eliminar, btn_actualizar;
+    private TextView tv_resultado;
+    boolean inputEt=false;
+    boolean inputEd=false;
+    boolean input1=false;
+    int resultadoInsert=0;
+
+    String senal = "";
+    String codigo = "";
+    String descripcion = "";
+    String precio = "";
+
+    //QUITAR COMENTARIOS
+    /*MantenimientoMySQL manto = new MantenimientoMySQL();
+    Dto datos = new Dto();*/
+
+    //Banderas para saber estados de métodos del CRUD.
+    boolean estadoGuarda = false;
+    boolean estadoEliminar = false;
+
+    AlertDialog.Builder dialogo;
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            new android.app.AlertDialog.Builder(this)
+                    .setIcon(R.drawable.ic_close)
+                    .setTitle("Advertencia")
+                    .setMessage("¿Realmente desea salir?")
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {//un listener que al pulsar, cierre la aplicacion
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            MainActivity.this.finishAffinity();
+                        }
+                    })
+                    .show();
+            return true;
+        }
+        //para las demas cosas, se reenvia el evento al listener habitual
+        return super.onKeyDown(keyCode, event);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_back));
+        toolbar.setTitleTextColor(getResources().getColor(R.color.mycolor1));
+        toolbar.setTitleMargin(0, 0, 0, 0);
+        toolbar.setSubtitle("CRUD MySQL~2019");
+        toolbar.setSubtitleTextColor(getResources().getColor(R.color.mycolor));
+        toolbar.setTitle("Alba García");
         setSupportActionBar(toolbar);
+
+        ///y esto para pantalla completa (oculta incluso la barra de estado)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        et_codigo = (EditText) findViewById(R.id.et_codigo);
+        et_descripcion = (EditText) findViewById(R.id.et_descripcion);
+        et_precio = (EditText) findViewById(R.id.et_precio);
+        btn_guardar = (Button) findViewById(R.id.btn_guardar);
+        btn_consultaCodigo = (Button) findViewById(R.id.btn_consultaCodigo);
+        btn_consultaDescripcion = (Button) findViewById(R.id.btn_consultaDescripcion);
+        btn_eliminar = (Button) findViewById(R.id.btn_eliminar);
+        btn_actualizar = (Button) findViewById(R.id.btn_actualizar);
+       // tv_resultado = (TextView) findViewById(R.id.tv_resultado);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogConfirmacion();
+            }
+        });
+
+        /******************************************************************/
+        //BLOQUE DE CÓDIGO PARA MOSTRAR DATOS DE LA BUSQUEDA//
+        try {
+            Intent intent = getIntent();
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+
+                senal = bundle.getString("senal");
+                codigo = bundle.getString("codigo");
+                descripcion = bundle.getString("descripcion");
+                precio = bundle.getString("precio");
+                if (senal.equals("1")) {
+                    et_codigo.setText(codigo);
+                    et_descripcion.setText(descripcion);
+                    et_precio.setText(precio);
+                    //finish();
+                }else if(senal.equals("2")){
+
+                }
+            }
+        }catch (Exception e){
+        }
+        /******************************************************************/
+
+
+        btn_guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(et_codigo.getText().toString().length()==0){
+                    et_codigo.setError("Campo obligatorio");
+                    inputEt = false;
+                }else {
+                    inputEt=true;
+                }
+                if(et_descripcion.getText().toString().length()==0){
+                    et_descripcion.setError("Campo obligatorio");
+                    inputEd = false;
+                }else {
+                    inputEd=true;
+                }
+                if(et_precio.getText().toString().length()==0){
+                    et_precio.setError("Campo obligatorio");
+                    input1 = false;
+                }else {
+                    input1=true;
+                }
+                if (inputEt && inputEd && input1){
+                    String codigo = et_codigo.getText().toString();
+                    String descripcion = et_descripcion.getText().toString();
+                    String precio = et_precio.getText().toString();
+                    //ARREGLAR COMENTARIO manto.guardar(MainActivity.this, codigo, descripcion, precio);
+
+                    /*
+                    estadoGuarda = manto.guardar1(Main2Activity.this, codigo, descripcion, precio);
+                    if(estadoGuarda){
+                        Toast.makeText(Main2Activity.this, "Registro Almacenado Correctamente.", Toast.LENGTH_SHORT).show();
+                        limpiarDatos();
+                        et_codigo.requestFocus();
+                    }*/
+
+                }
+            }
+        });
+
+        //Evento clic del botón eliminar.
+        btn_eliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(et_codigo.getText().toString().length()==0){
+                    et_codigo.setError("campo obligatorio");
+                    inputEt = false;
+                }else {
+                    inputEt=true;
+                }
+
+                if(inputEt){
+                    String codigo = et_codigo.getText().toString();
+                    //ARREGLAR COMENTARIO  manto.eliminar(MainActivity.this, codigo);
+
+                    limpiarDatos();
+                    et_codigo.requestFocus();
+                    /*
+                    if(estadoEliminar){
+                        Toast.makeText(Main2Activity.this, "Registro Eliminado correctamente.", Toast.LENGTH_SHORT).show();
+                        limpiarDatos();
+                    }else{
+                         Toast toast = Toast.makeText(getApplicationContext(), "--> Nothing." +
+                                        "\nNo hay información que eliminar.", Toast.LENGTH_LONG);
+                                toast.setGravity(Gravity.CENTER, 0, 0);
+                                toast.show();
+                        limpiarDatos();
+                    }*/
+                }
+            }
+        });
+
+        btn_consultaCodigo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Begin...
+                if(et_codigo.getText().toString().length()==0){
+                    et_codigo.setError("campo obligatorio");
+                    inputEt = false;
+                }else {
+                    inputEt=true;
+                }
+
+                if(inputEt) {
+                    String codigo = et_codigo.getText().toString();
+                    //ARREGLAR COMENTARIO manto.consultarCodigo(MainActivity.this, codigo);
+                    et_codigo.requestFocus();
+                }
+                //End
+
+            }
+        });
+
+        btn_consultaDescripcion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(et_descripcion.getText().toString().length()==0){
+                    et_descripcion.setError("Campo obligatorio");
+                    inputEd = false;
+                }else {
+                    inputEd=true;
+                }
+                if(inputEd){
+                    String descripcion = et_descripcion.getText().toString();
+                    //datos.setDescripcion(descripcion);
+                    //ARREGLAR COMENTARIO manto.consultarDescripcion(MainActivity.this, descripcion);
+                    et_descripcion.requestFocus();
+                    //Hilo();
+
+                }
+            }
+        });
+
+        btn_actualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(et_codigo.getText().toString().length()==0){
+                    et_codigo.setError("campo obligatorio");
+                    inputEt = false;
+                }else {
+                    inputEt=true;
+                }
+
+                if(inputEt) {
+
+                    String cod = et_codigo.getText().toString();
+                    String descripcion = et_descripcion.getText().toString();
+                    String precio = et_precio.getText().toString();
+
+                    //QUITAR COMENTARIOS
+                    /*datos.setCodigo(Integer.parseInt(cod));
+                    datos.setDescripcion(descripcion);
+                    datos.setPrecio(Double.parseDouble(precio));
+                    manto.modificar(MainActivity.this, datos);*/
+                    limpiarDatos();
+                    et_codigo.requestFocus();
+                }
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +281,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void limpiarDatos(){
+        et_codigo.setText(null);
+        et_descripcion.setText(null);
+        et_precio.setText(null);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,10 +304,96 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_limpiar) {
+            et_codigo.setText(null);
+            et_descripcion.setText(null);
+            et_precio.setText(null);
+            return true;
+        }else if(id == R.id.action_listaArticulos){
+            //Intent spinnerActivity = new Intent(MainActivity.this, Consulta_RecyclerView.class);
+            //startActivity(spinnerActivity);
+            return true;
+        }else if(id == R.id.action_salir){
+            DialogConfirmacion();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+    private void DialogConfirmacion(){
+        //startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        String mensaje = "¿Realmente desea salir?";
+        dialogo = new AlertDialog.Builder(MainActivity.this);
+        dialogo.setIcon(R.drawable.ic_close);
+        dialogo.setTitle("Advertencia");
+        dialogo.setMessage(mensaje);
+        dialogo.setCancelable(false);
+        dialogo.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogo, int id) {
+                /*Intent intent = new Intent(DashboardLuces.this, luces_control_sms.class);
+                startActivity(intent);*/
+                MainActivity.this.finishAffinity();
+                //Main2Activity.this.finish();
+            }
+        });
+        dialogo.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogo, int id) {
+                Toast.makeText(getApplicationContext(), "Operación Cancelada.", Toast.LENGTH_LONG).show();
+            }
+        });
+        dialogo.show();
+    }
+
+    //Creación de HILOS
+    void Hilo(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i=1; i<=1; i++){
+                    demora();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String cod = getSharedCodigo(MainActivity.this);
+                        String des = getSharedDescripcion(MainActivity.this);
+                        String pre = getSharedPrecio(MainActivity.this);
+
+                        et_codigo.setText(cod);
+                        et_descripcion.setText(des);
+                        et_precio.setText(pre);
+
+                        //Toast.makeText(Main2Activity.this, "Código: "+cod + "\nPrecio: "+pre + "\nDescripción: "+des, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }).start();
+    }
+
+    private void demora(){
+        try{
+            Thread.sleep(1000);
+        }catch (InterruptedException e){}
+    }
+
+
+    public String getSharedCodigo(Context context) {
+        SharedPreferences preferences = context.getSharedPreferences("profeGamez", MODE_PRIVATE);
+        String codigo = preferences.getString("codigo","0");
+        return codigo;   //return preferences.getString("tiempo", "Sin configurar.");
+    }
+
+    public String getSharedDescripcion(Context context) {
+        SharedPreferences preferences = context.getSharedPreferences("profeGamez", MODE_PRIVATE);
+        String descripcion = preferences.getString("descripcion","Sin descripción");
+        return descripcion;   //return preferences.getString("tiempo", "Sin configurar.");
+    }
+
+    public String getSharedPrecio(Context context) {
+        SharedPreferences preferences = context.getSharedPreferences("profeGamez", MODE_PRIVATE);
+        String precio = preferences.getString("precio","0.0");
+        return precio;   //return preferences.getString("tiempo", "Sin configurar.");
+    }
+
+
 }
