4a206925d46909b6611e12cad7f8e9d5a287658f
yessyGarcia <yesseniagarcia989@gmail.com>
2019-10-31 21:50:16 -0600

    CLASES

---
+++ app/src/main/java/com/example/crud_msql/Config.java
@@ -0,0 +1,17 @@
+package com.example.crud_msql;
+
+public class Config {
+
+    public static final String urlGuardar = "http://mjgl.com.sv/mysqlcrud/guardar.php";                               //Guardar datos DB.
+    public static final String urlEliminar = "http://mjgl.com.sv/mysqlcrud/eliminar.php";                             //Eliminar datos DB.
+    public static final String urlActualizar = "http://mjgl.com.sv/mysqlcrud/actualizar.php";                          //Actualizar datos DB.
+
+    public static final String urlConsultaApiMySQLi = "http://mjgl.com.sv/mysqlcrud/Api.php";                          //Ver todos los registros DB.
+    public static final String urlConsultaApiPDO = "http://mjgl.com.sv/mysqlcrud/buscarAll.php";                       //Ver todos los registros DB.
+
+    public static final String urlConsultaCodigo = "http://mjgl.com.sv/mysqlcrud/buscarArticulosCodigo.php";            //Busquedas por código.
+    public static final String urlConsultaDescripcion = "http://mjgl.com.sv/mysqlcrud/buscarArticulosDescripcion.php";  //Busquedas por descripción.
+    public static final String urlConsultaAllArticulos = "http://mjgl.com.sv/mysqlcrud/buscarArticulos.php";            //Devuelve todos los registros de la tabla MySQL.
+
+
+}
